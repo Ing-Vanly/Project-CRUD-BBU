f13107f4997e0821b0d8bb98268d2c6d8fe5e3e4
Ing Vanly <ingvanly168@gmail.com>
2025-10-20 10:29:02 +0700

    Made change with the route

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,18 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 
 // Route::middleware(['auth'])->group(function () {
+    Route::get('/', function () {
+        return view('auth.login');
+    });
+    Auth::routes();
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+    Route::resource('/role', RoleController::class);
+    Route::resource('/user', UserController::class);
+    Route::resource('/post', PostController::class);
+    Route::post('/post/{id}/toggle-published', [PostController::class, 'togglePublished'])->name('post.toggle-published');
+// });
+
+// Route::prefix('admin')->group(function () {
 //     Route::get('/', function () {
 //         return view('auth.login');
 //     });
@@ -17,15 +29,3 @@ use App\Http\Controllers\UserController;
 //     Route::resource('/post', PostController::class);
 //     Route::post('/post/{id}/toggle-published', [PostController::class, 'togglePublished'])->name('post.toggle-published');
 // });
-
-Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('auth.login');
-    });
-    Auth::routes();
-    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
-    Route::resource('/role', RoleController::class);
-    Route::resource('/user', UserController::class);
-    Route::resource('/post', PostController::class);
-    Route::post('/post/{id}/toggle-published', [PostController::class, 'togglePublished'])->name('post.toggle-published');
-});
